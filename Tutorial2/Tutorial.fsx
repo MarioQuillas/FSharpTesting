c574fe1bdf89865309368179859ea6c2d54d9529
Mario <mqs@melix.org>
2017-04-29 15:14:59 +0200

    parsing with regex is done
    one part of the scripting is done, starting to move the code to a reusable function

--- Tutorial2/Tutorial.fsx
+++ Tutorial2/Tutorial.fsx
@@ -5,25 +5,32 @@ open System.Text.RegularExpressions
 
 let regexThumb = Regex("<a[^>]*><img src=\"([^\"]*)\".*>(.*)")
 
-let m = regexThumb.Match("""<a href="https://dvd.netflix.com/Movie/Nerve/80103475"><img src="//secure.netflix.com/us/boxshots/small/80103475.jpg"/></a><br>After becoming involved in an online version of "Truth or Dare," young Vee soon realizes that the game is anything but an innocent lark as her audience of anonymous "watchers" begins to manipulate her life in dangerous ways.
-""")
-
-m.Success
-m.Groups.Count
-m.Groups.[0].Value
-m.Groups.[1].Value
-m.Groups.[2].Value
-
 // this two doesn't exist but we get an empty value when calling them and not an exception (Option or Maybe)
 //m.Groups.[3].Value
-//m.Groups.[4].Value
+//let mm = regexThumb.Match("")
+//let ff = mm.Groups.[4].Value
 
 type Netflix = 
     XmlProvider<"http://dvd.netflix.com/Top100RSS">
+
+type MovieBasics = 
+    {
+        Title : string
+        Summary : string
+        Thumbnail : option<string>
+    }
+      
 
 let top = Netflix.GetSample()
 top.Channel.Title
 
 for it in top.Channel.Items do
-    printfn "%s\n%s\n" it.Title.String.Value it.Description
+    let m = regexThumb.Match(it.Description)
+    let descr, thumb = 
+        if m.Success then
+            m.Groups.[2].Value,
+            Some(m.Groups.[1].Value)
+        else
+            it.Description, None
 
+    printfn "%s (%A)\n%s\n" it.Title.String.Value thumb descr
