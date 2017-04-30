0f019c7f09e9b2ede83d59bb55ff6a70ffc9033e
Mario <mqs@melix.org>
2017-04-30 19:21:15 +0200

    Adding C# test project
    saving F# code before refactoring for testing

---
+++ MovieNews.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MovieNews.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MovieNews.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c2a2b85-4036-4b22-8f50-4588cc50cd03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MovieNews.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieNews.Data;
+
+namespace MovieNews.Tests
+{
+    [TestClass]
+    public class MovieDataTests
+    {
+        [TestMethod]
+        public void CanParseNetflixTests()
+        {
+            //var toto = Netflix.get
+        }
+    }
+}
