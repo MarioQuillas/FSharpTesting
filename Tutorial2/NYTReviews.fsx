284e083a6b994db7701fa0400cb0ad6ed776898b
Mario <mqs@melix.org>
2017-04-30 15:55:55 +0200

    refactor to a function
    then using option.map

--- Tutorial2/NYTReviews.fsx
+++ Tutorial2/NYTReviews.fsx
@@ -1,23 +1,35 @@
 ﻿#r @"../packages/FSharp.Data.2.3.3/lib/net40/FSharp.Data.dll"
 open FSharp.Data
 
-
-
 type NYT =
     JsonProvider<"http://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=9b917cfa4dcc4e639256a20c700670be&query=paddington">
 
 let baseUrl = "http://api.nytimes.com/svc/movies/v2/reviews/search.json"
 let apiKey = "9b917cfa4dcc4e639256a20c700670be"
 
-let query = "the social network"//"paddington"
+type Review =
+    {
+        Published : System.DateTime
+        Summary : string
+        Link : string
+        LinkText : string
+    }
 
-let q = ["api-key", apiKey; "query", query]
-let response = Http.RequestString(baseUrl,q)
-let nyt = NYT.Parse(response)
+let tryPickReviewByName name =
+    let q = ["api-key", apiKey; "query", name]
+    let response = Http.RequestString(baseUrl,q)
+    let nyt = NYT.Parse(response)
 
-let reviewOpt = nyt.Results |> Seq.tryFind(fun r ->
-    System.String.Equals(r.DisplayTitle, query, System.StringComparison.InvariantCultureIgnoreCase))
+    let reviewOpt = nyt.Results |> Seq.tryFind(fun r ->
+        System.String.Equals(r.DisplayTitle, name, System.StringComparison.InvariantCultureIgnoreCase))
 
-match reviewOpt with
-| Some r -> printfn "%s" r.Headline
-| None -> printfn "No review"
+    match reviewOpt with
+    | Some r ->
+        Some {
+                Published = r.PublicationDate
+                Summary = r.SummaryShort
+                Link = r.Link.Url
+                LinkText = r.Link.SuggestedLinkText
+            }
+                
+    | None -> None
