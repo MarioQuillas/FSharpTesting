8fe04461526a047a4b096fcb6b17a891e7d90f4c
Mario <mqs@melix.org>
2017-04-30 19:10:31 +0200

    moving to a f# library

---
+++ MovieNews.Data/Netflix.fs
@@ -0,0 +1,29 @@
+﻿module MovieNews.Data.Netflix
+
+open FSharp.Data
+open System.Text.RegularExpressions
+
+let regexThumb = Regex("<a[^>]*><img src=\"([^\"]*)\".*>(.*)")
+
+type Netflix = 
+    XmlProvider<"http://dvd.netflix.com/Top100RSS">
+
+let getTop100() = 
+    let top = Netflix.GetSample()
+    [
+        for it in top.Channel.Items ->
+            let m = regexThumb.Match(it.Description)
+            let descr, thumb = 
+                if m.Success then
+                    m.Groups.[2].Value,
+                    Some(m.Groups.[1].Value)
+                else
+                    it.Description, None
+            {
+                Title = it.Title.String.Value
+                Summary = descr
+                Thumbnail = thumb
+            }
+    ]
+
+
