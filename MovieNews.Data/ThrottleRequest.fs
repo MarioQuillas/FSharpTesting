24e64f15cbd469b62387aa09fc4fec7530a1bd47
Mario <mqs@melix.org>
2017-05-01 15:57:41 +0200

    creating a throttle agent
    there is a build error that is going to be corrected in a second. This commit just shows how to create a single agent

---
+++ MovieNews.Data/ThrottleRequest.fs
@@ -0,0 +1,23 @@
+﻿module MovieNews.Data.Utils
+open FSharp.Data
+
+type ThrottleMessage =
+    {
+        Url : string
+        Query : seq<string*string>
+        Reply : AsyncReplyChannel<string>
+    }
+
+let agent = MailboxProcessor<ThrottleMessage>.Start(fun inbox -> 
+    async {
+        while true do
+            let! req = inbox.Receive()
+            let sw = System.Diagnostics.Stopwatch.StartNew()
+            let! res =
+                Http.AsyncRequestString(req.Url, List.ofSeq req.Query)
+            req.Reply.Reply(res)
+            let sleep = delay - (int sw.ElapsedMilliseconds)
+            if sleep > 0 then do! Async.Sleep(sleep)
+    }
+)
+
