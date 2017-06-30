37a3dee30e13d93ffe598f6b4498208ae08fa712
Mario <mqs@melix.org>
2017-06-30 20:16:01 +0200

    testing some Rx .net

--- CSharpFSharpTranslate/Program.cs
+++ CSharpFSharpTranslate/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,16 @@ namespace CSharpFSharpTranslate
     {
         static void Main(string[] args)
         {
+            var toto = Observable.Return(42);
+            toto.Subscribe(x => { }, x => { }, () => { });
+
+            toto.Subscribe
+            (
+                x => Console.WriteLine("OnNext: {0}", x),
+                ex => Console.WriteLine("OnError: {0}", ex),
+                () => Console.WriteLine("OnCompleted")
+            );
+
         }
     }
 }
